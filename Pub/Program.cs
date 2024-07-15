135e60f6634d7163256b8c77537db90d18a1e869
01010011011001010110010001110100 <moisesgironarias@gmail.com>
2024-07-14 21:19:27 -0400

    Inital Commit

---
+++ Pub/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+public class App {
+    private ConnectionMultiplexer Redis;
+
+    public App(){
+        Redis = ConnectionMultiplexer.Connect(new ConfigurationOptions(){
+            EndPoints =  { Environment.GetEnvironmentVariable("DOTNET_REDIS_ID")! },
+            Password = Environment.GetEnvironmentVariable("DOTNET_REDIS_PASSWORD")!
+        });
+
+        ISubscriber pub = Redis.GetSubscriber();
+        pub.Publish("messages", "HOLAAA, ESTE ES UN MENSJAE DESDE EL PUBLICADOR");
+    }
+
+    public static void Main(string[] args){
+        var obj = new App();
+        while(true){}
+    }
+}
---
+++ Sub/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+public class App {
+    private ConnectionMultiplexer Redis;
+
+    public App(){
+        Redis = ConnectionMultiplexer.Connect(new ConfigurationOptions(){
+            EndPoints =  { Environment.GetEnvironmentVariable("DOTNET_REDIS_ID")! },
+            Password = Environment.GetEnvironmentVariable("DOTNET_REDIS_PASSWORD")!
+        });
+
+        ChannelMessageQueue Channel = Redis.GetSubscriber().Subscribe("messages");
+        Channel.OnMessage(message => {
+           Console.WriteLine((string)message.Message!);
+        });
+    }
+
+    public static void Main(string[] args){
+        var obj = new App();
+        while(true){}
+    }
+}
